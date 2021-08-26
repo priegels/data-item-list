16b23417a1c6f5ebb537e4a4e5263535b929376f
priegels <88594441+priegels@users.noreply.github.com>
2021-08-26 18:01:41 +0200

    Created IIFE + turned for loop into forEach function
    Also created 2 experimental bonus lines: tagged "//does this make sense?"

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,35 @@
+//attempting to wrap my pokemonList in an IIFE
+let pokemonRepository = (function () {
+  let pokemonList = [
+    { name: 'Bulbasaur', height: 0.71, types: ['grass', 'poison']},
+    { name: 'Ivysaur', height: 1, types: ['grass', 'poison']},
+    { name: 'Venusaur', height: 2, types: ['grass', 'poison']},
+  ];
 
-let pokemonList = [
-  { name: 'Bulbasaur', height: 0.71, types: ['grass', 'poison']},
-  { name: 'Ivysaur', height: 1, types: ['grass', 'poison']},
-  { name: 'Venusaur', height: 2, types: ['grass', 'poison']}
-];
-
-
-//loop that lists every pokemon with their respective height + types
-//added a condition to display great height (using <b>, might be changed later on)
-//<p> is used to make it have a list optic as opposed to just having everything in one line of text
-for (let i = 0; i < pokemonList.length; i++) {
-  if (pokemonList[i].height >= 1.5) {
-    document.write('<p>' + pokemonList[i].name + ' (height: ' + pokemonList[i].height + 'm' + ' - <b>Wow, that\'s big!</b>' + '; types: ' + pokemonList[i].types + ') </p>');
-  } else {
-    document.write('<p>' + pokemonList[i].name + ' (height: ' + pokemonList[i].height + 'm' + '; types: ' + pokemonList[i].types + ') </p>');
+//does this make sense???
+  function add(pokemon) {
+    if (typeof(pokemon) === object) {
+    pokemonList.push(pokemon);
   }
 }
+
+  function getAll() {
+    return pokemonList;
+  }
+
+  return {
+    add: add,
+    getAll: getAll
+  };
+})();
+
+//loop that lists every pokemon with their respective height + types
+//<p> is used to create a list as opposed to just having everything in one line of text
+
+// new forEach function() for task 1.5 with called IIFE function
+pokemonRepository.getAll().forEach(function(pokemon) {
+  document.write('<p>' + pokemon.name + ' (height: ' + pokemon.height + 'm' + '; types: ' + pokemon.types + ') </p>');
+});
+
+// does this make sense??
+alert(Object.keys(pokemonRepository.getAll()));
