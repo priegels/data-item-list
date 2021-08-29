4d9a8348db64a9425121842e6f2be7fd7b1ca8c9
priegels <88594441+priegels@users.noreply.github.com>
2021-08-29 18:00:14 +0200

    Added EventListener that logs the selected object to the console

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-//IIFE containing Repository of Pokemon with height, types and eventually moves
+//Provides array of Pokémon
 let pokemonRepository = (function () {
   let pokemonList = [
     { name: 'Bulbasaur', height: 0.71, types: ['grass', 'poison']},
@@ -6,32 +6,47 @@ let pokemonRepository = (function () {
     { name: 'Venusaur', height: 2, types: ['grass', 'poison']},
   ];
 
-//function that only allows the addition of new pokemon if the pokemon is an object
+//Function that only allows the addition of new Pokémon if the Pokémon is an object
   function add(pokemon) {
-    if (typeof(pokemon) === object) {
+    if (typeof(pokemon) === 'object' && 'name' in pokemon) {
     pokemonList.push(pokemon);
+  } else {
+      console.log('error')
   }
 }
 
+//Returns list of Pokémon
   function getAll() {
     return pokemonList;
   }
 
+//Lists Pokémon in repository as buttons
   function addListItem(pokemon) {
     let pokemonUList = document.querySelector('.pokemon-list');
     let pokemonListItem = document.createElement('li');
     let button = document.createElement('button');
     button.innerText = pokemon.name;
     button.classList.add('button-pokemon');
+//Event Function to happen on click on button
+    button.addEventListener('click', function(event) {
+      showDetails(pokemon);
+    })
 
     pokemonListItem.appendChild(button);
     pokemonUList.appendChild(pokemonListItem);
   }
 
+//Shows details of selected Pokémon on click
+  function showDetails(pokemon) {
+    console.log(pokemon);
+  }
+
+//Function returns
   return {
     add: add,
     getAll: getAll,
-    addListItem: addListItem
+    addListItem: addListItem,
+    showDetails: showDetails
   };
 })();
 
