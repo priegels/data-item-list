892c935e3d4d2f0cfd40e67a0484e3b91165db49
priegels <88594441+priegels@users.noreply.github.com>
2021-09-09 12:44:18 +0200

    added function for back to top button

--- js/scripts.js
+++ js/scripts.js
@@ -140,3 +140,25 @@ pokemonRepository.loadList().then(function() {
     pokemonRepository.addListItem(pokemon);
   });
 });
+
+//Get the button:
+mybutton = document.getElementById("btn-back-to-top");
+
+// When the user scrolls down 200px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+mybutton.addEventListener("click", topFunction);
+
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
